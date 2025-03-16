cdc40e019e0a0cdcbe52674f074149cd940efe9c
Michal Uryč <michal.uryc@seznam.cz>
2025-03-16 10:46:37 +0100

    čw

--- Lekce_5/komprehence.py
+++ Lekce_5/komprehence.py
@@ -4,3 +4,10 @@ adjectives_plus_word = []
 
 [adjectives_plus_word.append(adjective + " " + word) for adjective in adjectives]
 print(adjectives_plus_word)
+
+fib_numbers = [0, 1]
+fib_number = 15
+
+while len(fib_numbers) <=14:
+    fib_numbers.append(fib_numbers[-1] + fib_numbers[-2])
+print(fib_numbers)
